d15cb1b7f07f06e8afbcd2b130626cf221fef467
KhoaBH <lamanhkhoa2004@gmail.com>
2024-04-18 12:26:26 +0700

    Commit lan 1

--- main/main.php
+++ main/main.php
@@ -1,58 +1,169 @@
+<?php
+    session_start();
+    if(isset($_SESSION['user'])&&($_SESSION['user']!="")){
+        $user=$_SESSION['user'];
+    }
+    if(isset($_SESSION['password'])&&($_SESSION['password']!="")){
+        $user=$_SESSION['password'];
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lí sổ đầu bài</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style_main.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="wrapper">
-    
-</div>
-<script>
-    // Disable form resubmission
-    if (window.history.replaceState) {
-        window.history.replaceState(null, null, window.location.href);
-    }
+    <header class="header">
 
-    // Function to show user not found message
-    function showUserNotFoundMessage() {
-        document.getElementById("user_not_found").style.display = "inline";
-    }
-    function showUserWrongPassWord() {
-        document.getElementById("wrong_password").style.display = "inline";
-    }
-</script>
+        <section class="flex">
+
+            <a href="home.html" class="logo">MDC Education</a>
+
+            <form action="search.html" method="post" class="search-form">
+                <input type="text" name="search_box" required placeholder="search courses..." maxlength="100">
+                <button type="submit" class="fas fa-search"></button>
+            </form>
+
+            <div class="icons">
+                <div id="menu-btn" class="fas fa-bars"></div>
+                <div id="search-btn" class="fas fa-search"></div>
+                <div id="user-btn" class="fas fa-user"></div>
+                <div id="toggle-btn" class="fas fa-sun"></div>
+            </div>
+        </section>
+
+    </header>
+
+    <div class="side-bar">
+
+        <div id="close-btn">
+            <i class="fas fa-times"></i>
+        </div>
+
+        <div class="profile">
+            <img src="images/pic-1.jpg" class="image" alt="">
+            <h3 class="name">Lâm Anh Khoa</h3>
+            <p class="role">Học sinh</p>
+            <a href="profile.html" class="btn">Thông tin cá nhân</a>
+        </div>
+
+        <nav class="navbar">
+            <a href="home.html"><i class="fas fa-home"></i><span>Trang chủ</span></a>
+            <a href="courses.html"><i class="fas fa-graduation-cap"></i><span>Thống kê</span></a>
+            <a href="teachers.html"><i class="fas fa-chalkboard-user"></i><span>Giáo viên</span></a>
+            <a href="index.php"><i class="fas fa-sign-out-alt"></i><span>Đăng xuất</span></a>
+        </nav>
+
+    </div>
+    <div class="schedule_content" style="display: flex">
+        <table>
+            <caption>Thời khóa biểu</caption>
+            <tr>
+                <th colspan="2">Thời gian</th>
+                <th>Thứ 2</th>
+                <th>Thứ 3</th>
+                <th>Thứ 4</th>
+                <th>Thứ 5</th>
+                <th>Thứ 6</th>
+            </tr>
+            <tr>
+                <th rowspan="5" class="schedule_time">Sáng</th>
+                <th class="schedule_time">Tiết 1<br>06:45 - 07:45</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+
+                <th class="schedule_time">Tiết 2<br>07:35 - 08:20</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th class="schedule_time">Tiết 3<br>08:40 - 09:25</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th class="schedule_time">Tiết 4<br>09:30 - 10:15</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th class="schedule_time">Tiết 5<br>10:25 - 11:10</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th rowspan="5" class="schedule_time">Chiều</th>
+                <th class="schedule_time">Tiết 6<br>12:30 - 13:15</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th class="schedule_time">Tiết 7<br>13:25 - 14:10</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th class="schedule_time">Tiết 8<br>14:15 - 15:00</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th class="schedule_time">Tiết 9<br>15:20 - 16:05</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th class="schedule_time">Tiết 10<br>16:10 - 16:45</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+        </table>
+    </div>
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+
 </body>
 </html>
-
-
-<?php
-session_start();
-$conn = mysqli_connect("localhost", "root", "", "login_db");
-if(isset($_POST['login_btn'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $sql = "SELECT * FROM login_info WHERE name=?";
-    $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, 's', $username);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-    if(mysqli_num_rows($result) == 0){ // If no rows were returned
-        echo "<script>showUserNotFoundMessage();</script>"; // Call JavaScript function to show the message
-    } else {
-        $row = mysqli_fetch_assoc($result);
-        $resultPassword = $row['password'];
-        if($password === $resultPassword){
-            $_SESSION['username']=$username;
-            $_SESSION['password']=$password;
-            header('Location: main.php');
-            exit();
-        } else {
-            echo "<script>showUserWrongPassWord();</script>";
-        }
-    }
-}
-?>
